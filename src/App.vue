231ba5af535e14eedc0b153a36382c67b76bf420
pepe sebrero <jsebrero@jssebrero.com>
2024-06-18 21:14:09 -0600

    feature: primera instantanea

---
+++ src/App.vue
@@ -0,0 +1,108 @@
+<script setup>
+
+    import {ref, reactive, onMounted, watch} from 'vue';
+    import {db} from './data/guitarras';
+    import Guitarra from './components/Guitarra.vue';
+    import Header from './components/Header.vue';
+    import Footer from './components/Footer.vue';
+
+    const guitarras = ref([]);
+    const carrito = ref([]);
+    const guitarra = ref({});
+
+    watch(carrito, () =>{
+        guardarLocalStorage();
+        },{
+            deep: true, 
+    });
+
+    onMounted(() => {
+        guitarras.value = db;
+        guitarra.value = db[3];
+
+        const carritoStorage = localStorage.getItem('carrito');
+        if(carritoStorage){
+            carrito.value = JSON.parse(carritoStorage);
+        }
+
+    });
+
+    const guardarLocalStorage = () =>{
+        localStorage.setItem('carrito', JSON.stringify(carrito.value));
+    }
+
+    const agregarCarrito = (guitarra) => {
+
+        const existeCarrito = carrito.value.findIndex(producto => producto.id === guitarra.id);
+        
+        if(existeCarrito < 0){
+            guitarra.cantidad = 1;
+            carrito.value.push(guitarra);
+            return;
+        }
+
+        carrito.value[existeCarrito].cantidad ++;
+    
+    }
+
+    const decrementarCantidad = (idProducto) => {
+
+        const index = carrito.value.findIndex(producto => producto.id === idProducto);
+
+        if(carrito.value[index].cantidad < 2){
+            return;
+        }
+        
+        carrito.value[index].cantidad--;
+
+    }
+
+    const incrementarCantidad = (idProducto) => {
+        const index = carrito.value.findIndex(producto => producto.id === idProducto);
+        
+        carrito.value[index].cantidad++;
+    }
+
+    const eliminarProducto = (idProducto) => {
+        carrito.value = carrito.value.filter(producto => producto.id !== idProducto);
+    }
+
+    const vaciarCarrito = () => {
+        carrito.value = [];
+    }
+
+</script>
+
+<template>
+
+    <Header 
+        :carrito = "carrito" 
+        :guitarra = "guitarra"
+        @decrementar-cantidad = "decrementarCantidad"
+        @incrementar-cantidad = "incrementarCantidad"
+        @agregar-carrito = "agregarCarrito"
+        @eliminar-producto = "eliminarProducto"
+        @vaciar-carrito = "vaciarCarrito"
+    />
+    
+    <main class="container-xl mt-5">
+        <h2 class="text-center">Nuestra Colección</h2>
+
+        <div class="row mt-5">
+
+            <Guitarra
+                v-for="guitarra in guitarras"
+                :guitarra = "guitarra"
+                @agregar-carrito = "agregarCarrito"
+            />
+
+        </div>
+    </main>
+
+    <Footer/>
+
+</template>
+
+<style scoped>
+
+</style>
